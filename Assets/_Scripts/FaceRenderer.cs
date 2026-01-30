7c8a5b232b7cfb6fb4e73f305f132330b18265ee
Ilay Sh <ilayshlomo@gmail.com>
2026-01-31 00:26:58 +0200

    adjusted game scene

---
+++ Assets/_Scripts/FaceRenderer.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class FaceRenderer : MonoBehaviour
+{
+    [SerializeField] private Sprite normal, soapy, masked, bloody;
+    [SerializeField] private Sprite maskedHalfPulledBloody, maskedHalfPulledNormal;
+}
