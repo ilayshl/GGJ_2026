a2ed2e162ab20ec630e4a941bc4cd8edad0be1ea
miladonn <55191447+miladonn@users.noreply.github.com>
2026-01-31 01:17:53 +0200

    fixed cursor

--- Assets/_Scripts/ITEMS/DragItem.cs
+++ Assets/_Scripts/ITEMS/DragItem.cs
@@ -5,15 +5,19 @@ using UnityEngine.InputSystem;
 
 public class DragItem : MonoBehaviour
 {
-    [SerializeField] private Texture2D cursorRegular;
-    [SerializeField] private Texture2D cursorHold;
+    [SerializeField] private FakeCursor _fakeCursor;
     public Vector2 cursorHotSpot = Vector2.zero;
     public CursorMode cursorMode = CursorMode.Auto;
     [SerializeField] private Camera cam;
     private PickableItemLocation pickedObject;
     private Vector3 offset;
     private bool isHeld;
-    
+
+    private void Start()
+    {
+        _fakeCursor.SetCursor(1);
+    }
+
     void Update()
     {
        
@@ -26,7 +30,8 @@ public class DragItem : MonoBehaviour
         {
             RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
             Debug.Log("casting ray");
-
+            _fakeCursor.SetCursor(2);
+          
             if (hit.collider && hit.collider.CompareTag("Pick"))
             {
                 Debug.Log("picked item");
@@ -35,19 +40,24 @@ public class DragItem : MonoBehaviour
                 offset = pickedObject.transform.position - (Vector3)mouseWorldPos;
                 isHeld = true;
             }
+            
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
             isHeld = false;
+            if (pickedObject == null)
+            {
+                _fakeCursor.SetCursor(1);
+                return;
+            }
                 pickedObject.ReturnToOrigin();
+                _fakeCursor.SetCursor(1);
                 
         }
 
         if (isHeld)
         {
             pickedObject.transform.position = (Vector3)mouseWorldPos + offset;
-           
-          
             
         }
     }
